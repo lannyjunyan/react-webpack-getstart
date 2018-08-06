08b8f2348094a1803f73d304292e501bc9bc9aa1
lannyjunyan <lannyjunyan@163.com>
2018-08-06 17:15:55 +0800

    tic

--- src/main.js
+++ src/main.js
@@ -5,28 +5,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 
-import ScreenLoginCtn from './containers/ScreenLoginCtn.js';
-import ScreenHomeCtn  from './containers/ScreenHomeCtn.js';
-import rootReducer from './reducers/rootReducer.js';
-
-console.log('ScreenLoginCtn:', ScreenLoginCtn);
-
-const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : (x)=>(x))
-);
-
-
 render(
-    <Provider store={store}>
-        <HashRouter>
-            <div style={{width: '100%', height: '100%', backgroundColor: '#bbb'}} >
-                <Route exact path="/" component={ ScreenLoginCtn }/>
-                <Route path="/home" component={ ScreenHomeCtn }/>
-            </div>
-        </HashRouter>
-    </Provider>
-    , document.getElementById('content')
+    <div>welcome</div>,
+    document.getElementById('content')
 );
 
 
